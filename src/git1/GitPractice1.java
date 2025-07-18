e5d8b3cc854fad718b6cac4717b0cfd1a4f55008
OhJunTaekk <mt3026super@naver.com>
2025-07-18 22:57:51 +0900

    v0.2

--- src/git1/GitPractice1.java
+++ src/git1/GitPractice1.java
@@ -4,5 +4,6 @@ public class GitPractice1 {
     public static void main(String[] args) {
         System.out.println("v0.1 입니다.");
         System.out.println("hotfix 1");
+        System.out.println("v0.2 입니다.");
     }
 }
