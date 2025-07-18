8bb8e911f7a5ff56f4a14ac1b0bcbacdef88297c
OhJunTaekk <mt3026super@naver.com>
2025-07-18 23:04:51 +0900

    feature2 4

--- src/git1/GitPractice1.java
+++ src/git1/GitPractice1.java
@@ -6,6 +6,7 @@ public class GitPractice1 {
         System.out.println("develop 1");
         System.out.println("feature2 1");
         System.out.println("feature2 2");
-        System.out.println("feature2 2");
+        System.out.println("feature2 3");
+        System.out.println("feature2 4");
     }
 }
