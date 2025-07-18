8551d9b94709d0e9a2ad0384f41ab025cb270e16
OhJunTaekk <mt3026super@naver.com>
2025-07-18 23:11:22 +0900

    feature1 1

--- src/git1/GitPractice1.java
+++ src/git1/GitPractice1.java
@@ -5,5 +5,6 @@ public class GitPractice1 {
         System.out.println("v0.1 입니다.");
         System.out.println("develop 1");
         System.out.println("develop 2");
+        System.out.println("feature1 1");
     }
 }
